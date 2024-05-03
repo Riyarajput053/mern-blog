a7fb06001b44f185a1d5d56f638b35cddef8207b
Riya Rajput <riyarrrajput053@gmail.com>
2024-05-04 00:37:29 +0530

    connected database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/User')
+.then(()=>{
+    console.log("database is connected")
+})
+.catch ((err)=>{
+    console.log(err);
+})
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000 !!');
