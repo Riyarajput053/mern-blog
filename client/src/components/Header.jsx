98cfbd659231dd70c9594a753c62dfc48393dfcd
Riya Rajput <riyarrrajput053@gmail.com>
2024-05-08 00:39:45 +0530

    added  signup page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,60 +1,62 @@
-import React from 'react'
-import { Navbar, TextInput, Button, NavbarCollapse, NavbarLink, NavbarToggle } from 'flowbite-react'
-import { Link, useLocation } from 'react-router-dom'
-import {AiOutlineSearch} from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa'
+import React from "react";
+import {
+  Navbar,
+  TextInput,
+  Button,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
-    const path = useLocation().pathname
+  const path = useLocation().pathname;
   return (
-    <Navbar className='border bg-2'>
-        <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-            <span className='px-2 py-1 bg-gradient-to-r from-[#265073] via-[#2D9596] to-[#9AD0C2] rounded-lg text-white '>Tech</span>
-            Blog
-        </Link>
+    <Navbar className="border bg-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-[#265073] via-[#2D9596] to-[#9AD0C2] rounded-lg text-white ">
+          Tech
+        </span>
+        Blog
+      </Link>
 
-        <form>
-            <TextInput
-            type="text"
-            placeholder='Search'
-            rightIcon={AiOutlineSearch}
-            className='hidden lg:inline '
-            />
-        </form>
-        <Button className="w-12 h-10 lg:hidden" color='gray' pill>
-            <AiOutlineSearch/>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search"
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline "
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
+      <NavbarCollapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+      </NavbarCollapse>
+
+      <div className="flex gap-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
         </Button>
-        <NavbarCollapse>
-            <Navbar.Link active={path === "/"} as={'div'}>
-                <Link to="/" >
-                Home
-                </Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === "/projects"} as={'div'}>
-                <Link to="/projects" >
-                Projects
-                </Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === "/about"} as={'div'}>
-                <Link to="/about" >
-                About
-                </Link>
-            </Navbar.Link>
-        </NavbarCollapse>
-
-        <div className='flex gap-2'>
-            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-                <FaMoon/>
-           </Button>
-           <Button className=' bg-[#2D9596]'>
-            Sign In
-           </Button>
-
-        </div>
-           <Navbar.Toggle />
-       
+        <Button className=" bg-[#2D9596]">Sign In</Button>
+      </div>
+      <Navbar.Toggle />
     </Navbar>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Label, TextInput,Button } from "flowbite-react";
 
 const SignUp = () => {
   return (
-    <div>
-      SignUp
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5" >
+        <div className="flex-1">
+        <Link to='/' className=' font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-[#265073] via-[#2D9596] to-[#9AD0C2] rounded-lg text-white '>Tech</span>
+            Blog
+        </Link>
+        <p className="text-sm mt-5">A Blog for technical information!</p>
+        </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value='Your username'/>
+              <TextInput type="text" placeholder="Username" id="username"/>
+            </div>
+            <div>
+              <Label value='Your email'/>
+              <TextInput type="text" placeholder="Email" id="email"/>
+            </div>
+            <div>
+              <Label value='Your password'/>
+              <TextInput type="text" placeholder="Password" id="password"/>
+            </div>
+            <Button className="px-2 py-1 bg-gradient-to-r from-[#265073] via-[#2D9596] to-[#9AD0C2] rounded-lg text-white " type="submit">Sign-Up</Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Already have an account ? </span>
+            <Link to='/sign-in' className="text-blue-500">Sign-In here</Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
