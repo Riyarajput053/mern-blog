89118f3d8b90f0ebbc13c578fc847a0defb4e7c9
Riya Rajput <riyarrrajput053@gmail.com>
2024-05-07 23:51:08 +0530

    added signup api route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/User')
-.then(()=>{
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/techBlog')
+.then(()=>{ 
     console.log("database is connected")
 })
 .catch ((err)=>{
@@ -14,8 +17,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/User')
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000 !!');
 });
-
+    
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
 
